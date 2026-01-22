c227f16c631dbd5bb826d4a02abba8a698141731
caique-98 <caiquebispobomfim.10@gmail.com>
2026-01-21 23:13:22 -0300

    ..

---
+++ index.js
@@ -0,0 +1,19 @@
+vel = 60
+
+console.log(`A velocidade do seu veiculo e de ${vel}Km/h.`)
+if (vel > 60){
+    console.log(`Voce ultrapassou a velocidade permitida. Voce foi multado!`)
+    console.log(`Reduza a velocidade por seguranca! Voce pode estar pagando sua divida online via link: www.bzbz.com.br.`)
+} else {
+    console.log(`Dirija sempre utilizando cinto de seguranca!`)
+}
+
+pais = 'USA'
+
+console.log(`Vivendo em ${pais}`)
+if (pais == 'Brasil'){
+    console.log(`Voce e bostileiro.`)
+} else{
+    console.log(`Voce e gringo.`)
+}
+
