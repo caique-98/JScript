0fd8faa481839c38a076b06a51ca2d65c35047ad
caique-98 <caiquebispobomfim.10@gmail.com>
2026-02-03 22:01:49 -0300

    Update index.js

--- repeticoes/index.js
+++ repeticoes/index.js
@@ -1,3 +1,18 @@
+console.log('Vai comecar...')
+
+for (c = 1; c<=10; c++){ // Modo mais recomendado e utilizado.
+    console.log(c)
+}
+
+console.log('Fim!')
+
+
+
+
+
+
+
+/*
 b = 1
 
 do {
@@ -6,11 +21,9 @@ do {
 } while (b <= 10)
 
 
-
-
 var c = 1
 
 while (c <= 10){
     console.log('Ola')
     c++ 
-}
+} */
