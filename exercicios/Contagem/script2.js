956f506f01d32baf0bcd422833d98a09197cf720
caique-98 <caiquebispobomfim.10@gmail.com>
2026-02-05 14:33:53 -0300

    .

---
+++ exercicios/Contagem/script2.js
@@ -0,0 +1,11 @@
+function calcular(){
+    ini = document.querySelector('input#inicio')
+    fim = document.querySelector('input#fim')
+    passo = document.querySelector('input#passo')
+
+    res = document.querySelector('div#res')
+
+    // A contagem deve comecar pela variavel inicio.
+    // A contagem deve ir da variavel inicio ate a variavel fim.
+    // A contagem deve ser feita conforme a variavel passo. Ex: se inicio for 1, fim for 10, e passo for 1. Entao... 1, 2, 3, 4, 5, 6, 7, 8, 9, 10. Agora se passo for 2: 1, 3, 5, 7, 9, 10.
+}
