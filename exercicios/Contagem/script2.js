722f53524064891a002f06f829da9651eb87c310
caique-98 <caiquebispobomfim.10@gmail.com>
2026-02-06 01:07:44 -0300

    .

--- exercicios/Contagem/script2.js
+++ exercicios/Contagem/script2.js
@@ -8,4 +8,6 @@ function calcular(){
     // A contagem deve comecar pela variavel inicio.
     // A contagem deve ir da variavel inicio ate a variavel fim.
     // A contagem deve ser feita conforme a variavel passo. Ex: se inicio for 1, fim for 10, e passo for 1. Entao... 1, 2, 3, 4, 5, 6, 7, 8, 9, 10. Agora se passo for 2: 1, 3, 5, 7, 9, 10.
+
+    
 }
---
+++ exercicios/Contagem/teste.js
@@ -0,0 +1,8 @@
+passo = 2
+inicio = 10
+fim = 20
+
+for(inicio.value >= 1; fim >= 20; passo++){
+    console.log('oi?')
+
+}
