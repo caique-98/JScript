1bcb4f73c4421050858cb004dc85abb00f462578
caique-98 <caiquebispobomfim.10@gmail.com>
2026-02-10 22:01:52 -0300

    .

---
+++ exercicios/tabuada/script.js
@@ -0,0 +1,11 @@
+function tabuada(){
+    let numb = document.getElementById('num')
+    let tab = document.getElementById('seltab')
+    if(numb.value.length == 0){
+        window.alert('Digite um numero valido!')
+    } else{
+        let n = Number(numb.value)
+    }
+    
+
+}
