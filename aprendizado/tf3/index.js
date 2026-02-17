bd98da5d3d6df260a100443388e8deba60280ec2
caique-98 <caiquebispobomfim.10@gmail.com>
2026-02-16 23:01:23 -0300

    .

--- aprendizado/tf3/index.js
+++ aprendizado/tf3/index.js
@@ -5,46 +5,54 @@ function enviar(){
     i1 = Number(i.value)
     s = document.querySelector('#isalar')
     s1 = Number(s.value)
+    s1.toFixed(2).replace('.', ',')
+    ss = parseFloat(s1)
     rs = document.getElementsByName('rads')
-
-    opts = [document.getElementsByTagName('option')[0]]
-    opts[1] = document.getElementsByTagName('option')[1]
-    opts[2] = document.getElementsByTagName('option')[2]
-    opts.push(document.getElementsByTagName('option')[3])
-    op = String(opts.value)
-
-    b = document.getElementsByTagName('body')[0]
-    b.style.background = 'gray'
-
+    selc = document.getElementById('isel')
+    
+    op = selc.value // Pegando a option pelo valor que o usuario seleciona no <select>.
 
     r = document.querySelector('div#res')
-
+    genero = ''
 
     
     if(n.value.length == 0 || i.value.length == 0 || s.value.length == 0){
         window.alert('nao')
-        genero = ''
+        
         
     } else if(n.value.length < 3 || n.value.length > 150){
         alert('?')
 
 
-    } else if(i.value < 0 || i.value > 150){
+    } else if(i.value < 0 || i.value > 128){
         alert('??')
 
     } else if(s.value <= 0){
         alert('???')
 
-    } else if (rs[0].checked && op.indexOf(0)){
-        r.innerHTML = `Ola, ${n1}. Como consta na verificacao, voce e um ser humano do sexo ${genero = 'masculino'}, com ${i1} anos, com salario estimado em ${s1} reais. Por fim, atualmente ${op[0] = 'solteiro(a)'}.`
-    } else{
-        r.innerHTML = `Ola, ${n1}. Como consta na verificacao, voce e um ser humano do sexo ${genero = "feminino"}, com ${i1} anos, com salario estimado em ${s1} reais. Por fim, atualmente ${op.indexOf(0)}.`
-        
-    }
-    r.style.textAlign = 'center'
-    r.style.width = '50vw'
-    r.style.marginTop = '20px'
-    r.style.padding = '20px'
-    r.style.font = '17pt Arial'
+    } else if(rs[0].checked){
+        r.innerHTML = `Olá, ${n1}. Como consta na verificação, você é do sexo ${genero = 'masculino'}, atualmente ${op}. Com idade atual de ${i1} anos. E renda auto-declarada em R$${ss.toFixed(2)}.`
 
-}
+
+        r.style.textAlign = 'JUSTIFY'
+        r.style.width = '50vw'
+        r.style.marginTop = '20px'
+        r.style.padding = '20px'
+        r.style.font = '17pt Arial'
+        r.style.background = 'gray'
+        r.style.color = 'white'
+    } else{
+        r.innerHTML = `Olá, ${n1}. Como consta na verificação, você é do sexo ${genero = 'feminino'}, atualmente ${op}. Com idade atual de ${i1} anos. E renda auto-declarada em R$${ss.toFixed(2)}.`
+        r.style.textAlign = 'JUSTIFY'
+        r.style.width = '50vw'
+        r.style.marginTop = '20px'
+        r.style.padding = '20px'
+        r.style.font = '17pt Arial'
+        r.style.background = 'purple'
+        r.style.color = 'white'
+    }
+
+    
+
+    }
+    
