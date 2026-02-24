b094283f4b12ba6d65e3e16923d7515fefd8afb1
caique-98 <caiquebispobomfim.10@gmail.com>
2026-02-24 00:00:54 -0300

    ..

---
+++ aprendizado/tf9/ine.js
@@ -0,0 +1,7 @@
+i = 1
+f = 50
+p = 2
+
+for(c = i; c <= f; c += p){
+    console.log(c)
+}
--- repeticoes/node_ce12334144717.tmp.js
+++
@@ -1,9 +0,0 @@
-a = 80000
-b = 200000
-taxa = 1.030
-ano = 1
-for(c = a; c = b; c *= taxa){
-    console.log(c)
-    console.log(ano)
-    ano++
-}
